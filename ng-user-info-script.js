1ce9e6516a1d37e5dfa84b7e35de7fc0a8a5f0e2
Octomorrow <octomorrow@gmail.com>
2019-05-14 08:21:49 +0700

    'Them user info edit va sign up'

--- ng-user-info-script.js
+++ ng-user-info-script.js
@@ -1,9 +1,30 @@
 $(function() {
+	var old_html_img_1 = $('.ng-group-icon-user-1').html();
+	var old_html_img_2 = $('.ng-group-icon-user-2').html();
     getAcceptedExtension = function(element) {
 		$('#inputFile1').attr('accept', '.jpg, .png');
-		$('#inputFile1').show();
-		$('#inputFile1').focus();
 		$('#inputFile1').click();
-		$('#inputFile1').hide();
+		// $('#inputFile1').show();
+		// $('#inputFile1').focus();
+		// $('#inputFile1').hide();
+	}
+	getimgsource = function(event) {
+		const window_width = $(window).width();
+		let order = 2;
+		if (window_width < 1200) {
+			order = 1;
+		}
+		let reader = new FileReader();
+		let file = event.files[0];
+		if(file) {
+			reader.readAsDataURL(event.files[0]);
+			reader.addEventListener('load', function() {
+			    var res = reader.result;
+			    let html_img = `<img src="${res}" class="ng-user-image">`;
+			    $(`.ng-group-icon-user-${order}`).html(html_img);
+			});
+		} else {
+			$(`.ng-group-icon-user-${order}`).html(old_html_img_[order]);
+		}
 	}
 });
